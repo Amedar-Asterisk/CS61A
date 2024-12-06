8d004a91fee2e41fed6658f28a992ae54d2a626d
ZRQ <DHO1371996217@outlook.com>
2024-12-06 15:52:18 +0800

    12/6 up to project1: hog

---
+++ lab01/return-and-print.py
@@ -0,0 +1,18 @@
+def welcome():
+    print("Go")
+    return "hello"
+
+
+def cal():
+    print("Bears")
+    return "world"
+
+
+welcome()
+print(welcome(), cal())
+
+
+### Go
+### Go
+### Bears
+### hello world
